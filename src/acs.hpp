fba4276e8a6ca5422d30fc1759a576e3fc8d8f93
John Scott <john.scott17@nhs.net>
2023-03-27 10:08:05 +0100

    Started reproducing the timings from before to double check performance baseline

--- src/acs.hpp
+++ src/acs.hpp
@@ -650,7 +650,7 @@ private:
 const std::string episodes_query{
     R"raw_sql(
 
-select
+select top 500000
 	episodes.*,
 	mort.REG_DATE_OF_DEATH as date_of_death,
 	mort.S_UNDERLYING_COD_ICD10 as cause_of_death,
