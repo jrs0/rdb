3fffa9fe19b37eb1998408db3ec3f78f6c9b807f
John Scott <john.scott17@nhs.net>
2023-03-30 13:28:45 +0100

    Tested that secondary procedures work

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,10 @@ int main() {
     episode.set_primary_diagnosis(parser.parse_diagnosis("I210"));
     episode.push_secondary_diagnosis(parser.parse_diagnosis("I220"));
     episode.push_secondary_diagnosis(parser.parse_diagnosis("I240"));
+
+    episode.set_primary_procedure(parser.parse_procedure("K432"));
+    episode.push_secondary_procedure(parser.parse_procedure("K111"));
+    episode.push_secondary_procedure(parser.parse_procedure("K221"));
+
     episode.print(parser, 2);
 }
