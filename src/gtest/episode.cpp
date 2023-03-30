0faf1e71c1eab2c89dfdcbf4be2d5c275f903574
John Scott <john.scott17@nhs.net>
2023-03-30 13:42:45 +0100

    Checked the procedures and diagnoses are parsed correctly

--- src/gtest/episode.cpp
+++ src/gtest/episode.cpp
@@ -8,14 +8,33 @@ TEST(Episode, SetDiagnosesAndProcedures) {
     Episode episode;
     ClinicalCodeParser parser{"../../opcs4.yaml", "../../icd10.yaml"};
 
+    // Add a selection of diagnoses
     episode.set_primary_diagnosis(parser.parse_diagnosis("I210"));
     episode.push_secondary_diagnosis(parser.parse_diagnosis("I220"));
     episode.push_secondary_diagnosis(parser.parse_diagnosis("I240"));
 
+    // Add a selection of procedures including a duplicate
     episode.set_primary_procedure(parser.parse_procedure("K432"));
     episode.push_secondary_procedure(parser.parse_procedure("K111"));
     episode.push_secondary_procedure(parser.parse_procedure("K221"));
+    episode.push_secondary_procedure(parser.parse_procedure("K221"));
 
+    // Check the primaries
     EXPECT_EQ(episode.primary_diagnosis().name(parser), "I21.0");
     EXPECT_EQ(episode.primary_procedure().name(parser), "K43.2");
+
+    // Check the secondaries
+    auto & secondary_diagnoses{episode.secondary_diagnoses()};
+    auto & secondary_procedures{episode.secondary_procedures()};
+
+    EXPECT_EQ(secondary_diagnoses.size(), 2);
+    EXPECT_EQ(secondary_procedures.size(), 3);
+
+    EXPECT_EQ(secondary_diagnoses[0].name(parser), "I22.0");
+    EXPECT_EQ(secondary_diagnoses[1].name(parser), "I24.0");
+
+    EXPECT_EQ(secondary_procedures[0].name(parser), "K11.1");
+    EXPECT_EQ(secondary_procedures[1].name(parser), "K22.1");
+    EXPECT_EQ(secondary_procedures[2].name(parser), "K22.1");
+
 }
