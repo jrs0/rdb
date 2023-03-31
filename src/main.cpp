3367d3f3b9b226cc203f69266e3dbc0a46d7dc3f
John Scott <john.scott17@nhs.net>
2023-03-31 11:15:26 +0000

    Added episode from sql row --getting sql type errors in the backend

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,14 @@ int main() {
     auto config{load_config_file("../../config.yaml")};
     auto parser{new_clinical_code_parser(config["parser"], lookup)};
     auto sql_connection{new_sql_connection(config["connection"])};
-    auto sql_query{make_acs_sql_query(config["sql_query"])};
+    auto sql_query{make_acs_sql_query(config["sql_query"], false)};
 
     std::cout << sql_query << std::endl;
     
     auto row{sql_connection.execute_direct(sql_query)};
+
+    Episode episode{row, parser};
+    episode.print(lookup);
     
     // Spell spell{rows, parser};
     // spell.print(parser);    
