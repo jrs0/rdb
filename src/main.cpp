425faf0696f73c67e7962950b5eaecf901c3e926
John Scott <john.scott17@nhs.net>
2023-03-31 10:33:34 +0000

    SQL query looks OK, time to test

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,16 @@
 #include "spell_rows.hpp"
 #include "spell.hpp"
 #include "config.hpp"
+#include "sql_query.hpp"
 
 int main() {
     auto lookup{new_string_lookup()};
     auto config{load_config_file("../../config.yaml")};
     auto parser{new_clinical_code_parser(config["parser"], lookup)};
 
+    std::cout << make_acs_sql_query(config["sql_query"]) << std::endl;
+
+    
     // Spell spell{rows, parser};
     // spell.print(parser);    
 }
--- src/sql_query.hpp
+++ src/sql_query.hpp
@@ -1,8 +1,16 @@
 #ifndef SQL_QUERY
-#define SQL_QUER
+#define SQL_QUERY
 
 #include "yaml.hpp"
 
+/**
+ * \brief Make the SQL query for the ACS dataset
+ *
+ * The config file is the "sql_query" block. It should contains primary_diagnosis
+ * and primary_procedure keys, and secondary_diagnoses and secondary_procedures
+ * lists. These are all column names, that will be mapped to the names used
+ * by the Episode constructor
+ */
 std::string make_acs_sql_query(const YAML::Node & config, bool with_mortality = true) {
 
     std::stringstream query;
@@ -11,33 +19,39 @@ std::string make_acs_sql_query(const YAML::Node & config, bool with_mortality = 
 
     if (config["result_limit"]) {
 	std::size_t result_limit{config["result_limit"].as<std::size_t>()};
-	std::cout << "Using result limit " << result_limit << std::endl;
 	query << "top " << result_limit << " ";
     }
     
     query << "episodes.* ";
     if (with_mortality) {
-	query << ", mort.REG_DATE_OF_DEATH as date_of_death,"
-	      << ", mort.S_UNDERLYING_COD_ICD10 as cause_of_death,"
+	query << ", mort.REG_DATE_OF_DEATH as date_of_death"
+	      << ", mort.S_UNDERLYING_COD_ICD10 as cause_of_death"
 	      << ", mort.Dec_Age_At_Death as age_at_death ";
     }
-    query << "from (select " 
+    query << std::endl
+	  << "from (select " 
 	  << "AIMTC_Pseudo_NHS as nhs_number,"
 	  << "AIMTC_Age as age_at_episode,"
 	  << "PBRspellID as spell_id,"
 	  << "StartDate_ConsultantEpisode as episode_start,"
 	  << "EndDate_ConsultantEpisode as episode_end,"
 	  << "AIMTC_ProviderSpell_Start_Date as spell_start,"
-	  << "AIMTC_ProviderSpell_End_Date as spell_end ";
+	  << "AIMTC_ProviderSpell_End_Date as spell_end,"
+	  << std::endl;
 
-    // Add all the diagnosis columns
-    auto diagnoses{expect_string_vector(config["parser_config"]["diagnoses"], "source_columns")};
-    for (const auto & diagnosis : diagnoses) {
-	query <<  "," << diagnosis;
+    // Add the primary diagnosis and procedure columns
+    query << config["primary_diagnosis"] << " as primary_diagnosis," << std::endl;
+    query << config["primary_procedure"] << " as primary_procedure " << std::endl;
+
+    // Get all the secondary diagnosis columns
+    std::size_t count{0};
+    for (const auto & diagnosis : config["secondary_diagnoses"]) {
+	query <<  "," << diagnosis << " as secondary_diagnosis_" << count++ << std::endl;
     }
-    auto procedures{expect_string_vector(config["parser_config"]["procedures"], "source_columns")};
-    for (const auto & procedure : procedures) {
-	query << "," << procedure;
+    count = 0;
+    for (const auto & procedure : config["secondary_procedures"]) {
+	query << "," << procedure << " as secondary_procedure_" << count++
+	      << std::endl;
     }
 
     query << " from abi.dbo.vw_apc_sem_001 "
