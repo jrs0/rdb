a5d3d17c72bf2972864b231d4724d4b3e13fa76e
John Scott <john.scott17@nhs.net>
2023-04-01 22:30:30 +0100

    Added more detail to test comment

--- src/gtest/parser.cpp
+++ src/gtest/parser.cpp
@@ -6,11 +6,30 @@
 
 /// This test currently fails because the file format
 /// is wrong (G06 is currently not ordered properly,
-/// opcs4.yaml needs fixing)
+/// opcs4.yaml needs fixing). The issue is that
+/// the categories are not ordered properly.
 TEST(ParserBoundaries, G069) {
     auto lookup{new_string_lookup()};
     auto config{load_config_file("../../config.yaml")};
     auto parser{new_clinical_code_parser(config["parser"], lookup)};
     auto code{parser->parse(CodeType::Procedure, "G069 ")};
+    EXPECT_FALSE(code.invalid());
+}
+
+/// Also incorrect index in file
+TEST(ParserBoundaries, W561) {
+    auto lookup{new_string_lookup()};
+    auto config{load_config_file("../../config.yaml")};
+    auto parser{new_clinical_code_parser(config["parser"], lookup)};
+    auto code{parser->parse(CodeType::Procedure, "  W56.1 ")};
+    EXPECT_FALSE(code.invalid());
+}
+
+/// Also incorrect index in file
+TEST(ParserBoundaries, W983) {
+    auto lookup{new_string_lookup()};
+    auto config{load_config_file("../../config.yaml")};
+    auto parser{new_clinical_code_parser(config["parser"], lookup)};
+    auto code{parser->parse(CodeType::Procedure, "W983")};
     EXPECT_FALSE(code.invalid());
 }
