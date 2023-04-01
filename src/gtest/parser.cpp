da0b837c1ddd21bd72999b11e75a39b617fe48ad
John Scott <john.scott17@nhs.net>
2023-04-01 22:06:19 +0100

    Added test to isolate failing case, and commented why it is failing

--- src/gtest/parser.cpp
+++ src/gtest/parser.cpp
@@ -4,11 +4,13 @@
 #include "../string_lookup.hpp"
 #include "../config.hpp"
 
-
+/// This test currently fails because the file format
+/// is wrong (G06 is currently not ordered properly,
+/// opcs4.yaml needs fixing)
 TEST(ParserBoundaries, G069) {
     auto lookup{new_string_lookup()};
     auto config{load_config_file("../../config.yaml")};
     auto parser{new_clinical_code_parser(config["parser"], lookup)};
     auto code{parser->parse(CodeType::Procedure, "G069 ")};
-    print(code, lookup);
+    EXPECT_FALSE(code.invalid());
 }
