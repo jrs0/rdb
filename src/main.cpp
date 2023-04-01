aee822d49aaeae9cad505113200d79386184ed2e
John Scott <john.scott17@nhs.net>
2023-04-01 21:24:58 +0100

    Started refactoring to always use shared_ptr to parser

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+//#include "acs.hpp"
+#include "clinical_code.hpp"
+#include "episode.hpp"
+#include "episode_row.hpp"
+#include "spell_rows.hpp"
+#include "spell.hpp"
+#include "config.hpp"
+#include "sql_query.hpp"
+#include "sql_connection.hpp"
+
+#include "cmdline.hpp"
+
+int main(int argc, char ** argv) {
+
+    auto lookup{new_string_lookup()};
+    auto config{load_config_file("../../config.yaml")};
+    auto parser{new_clinical_code_parser(config["parser"], lookup)};
+
+    
+    
+    //auto sql_connection{new_sql_connection(config["connection"])};
+    //auto sql_query{make_acs_sql_query(config["sql_query"], false, std::nullopt)};
+
+    
+    /*
+    std::cout << sql_query << std::endl;
+
+    try {
+	auto row{sql_connection.execute_direct(sql_query)};
+	while (true) {
+	    Episode episode{row, parser};
+	    episode.print(lookup);
+	    std::cout << std::endl;
+	    row.fetch_next_row();
+	}
+	    
+    } catch (const RowBufferException::NoMoreRows &) {
+	std::cout << "Finished fetching all rows" << std::endl;
+    }
+    */
+    
+
+}
