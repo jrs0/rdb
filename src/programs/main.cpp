6ac07a271313f930b479c5ccc2f92fe750e23636
John Scott <john.scott17@nhs.net>
2023-04-04 12:54:31 +0000

    main throws a ClinicalCode empty

--- src/programs/main.cpp
+++ src/programs/main.cpp
@@ -46,12 +46,35 @@ int main(int argc, char ** argv) {
 
     std::cout << sql_query << std::endl;
 
+    ClinicalCodeMetagroup acs{config["code_groups"]["acs"], lookup};
+    ClinicalCodeMetagroup pci{config["code_groups"]["pci"], lookup};
+    
     auto row{sql_connection.execute_direct(sql_query)};
     
     while (true) {
 	try {
 	    Patient patient{row, parser};
-	    patient.print(lookup);
+
+	    /// Is index event if there is a primary ACS or PCI
+	    /// in the _first_ episode of the spell
+	    auto is_acs_index_spell{[&](const Spell &spell) {
+		auto & episodes{spell.episodes()};
+		if (episodes.empty()) {
+		    return false;
+		}
+		auto & first_episode{episodes[0]};
+		auto primary_acs{acs.contains(first_episode.primary_diagnosis())};
+		auto primary_pci{acs.contains(first_episode.primary_procedure())};
+		return primary_acs or primary_pci;
+	    }};
+	    
+	    auto index_spells { patient.spells() | std::views::filter(is_acs_index_spell) };
+
+	    std::cout << "Patient = " << patient.nhs_number() << std::endl;
+	    for (const auto & spell : index_spells) {
+		spell.print(lookup, 4);
+	    }
+	    
 	} catch (const RowBufferException::NoMoreRows &) {
 	    std::cout << "Finished fetching all rows" << std::endl;
 	    break;
