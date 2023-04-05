a87d98238c8e5c5d9b70f45ac30fb299acb3dd6a
John Scott <john.scott17@nhs.net>
2023-04-05 17:45:47 +0100

    Merged latest version into R main

--- src/main_r.cpp
+++ src/main_r.cpp
@@ -15,9 +15,10 @@ void make_acs_dataset(const Rcpp::CharacterVector & config_path) {
     std::string config_path_str{Rcpp::as<std::string>(config_path)};
     
     try {
+
 	auto lookup{new_string_lookup()};
-	auto config{load_config_file(config_path_str)};
-        auto parser{new_clinical_code_parser(config["parser"], lookup)};
+	auto config{load_config_file("../../config.yaml")};
+	auto parser{new_clinical_code_parser(config["parser"], lookup)};
 	auto sql_connection{new_sql_connection(config["connection"])};
 	auto sql_query{make_acs_sql_query(config["sql_query"], false, std::nullopt)};
 
@@ -27,29 +28,29 @@ void make_acs_dataset(const Rcpp::CharacterVector & config_path) {
 	ClinicalCodeMetagroup pci{config["code_groups"]["pci"], lookup};
     
 	auto row{sql_connection.execute_direct(sql_query)};
+
+	std::vector<AcsRecord> acs_records;
+
+	auto print{config["print"].as<bool>()};
+
+	std::cout << "Started fetching rows" << std::endl;
     
 	while (true) {
 	    try {
 		Patient patient{row, parser};
 
-		/// Is index event if there is a primary ACS or PCI
-		/// in the _first_ episode of the spell
-		auto is_acs_index_spell{[&](const Spell &spell) {
-		    auto & episodes{spell.episodes()};
-		    if (episodes.empty()) {
-			return false;
-		    }
-		    auto & first_episode{episodes[0]};
-		    auto primary_acs{acs.contains(first_episode.primary_diagnosis())};
-		    auto primary_pci{pci.contains(first_episode.primary_procedure())};
-		    return primary_acs or primary_pci;
-		}};
-	    
-		auto index_spells { patient.spells() | std::views::filter(is_acs_index_spell) };
+		auto index_spells{get_acs_index_spells(patient.spells(), acs, pci)};
 
-		std::cout << "Patient = " << patient.nhs_number() << std::endl;
-		for (const auto & spell : index_spells) {
-		    spell.print(lookup, 4);
+		if (index_spells.empty()) {
+		    continue;
+		}
+
+		if (print) {
+		    std::cout << "Patient = " << patient.nhs_number() << std::endl;
+		}
+		for (const auto & index_spell : index_spells) {
+		    auto record{get_record_from_index_spell(patient, index_spell, lookup, print)};
+		    acs_records.push_back(record);
 		}
 	    
 	    } catch (const RowBufferException::NoMoreRows &) {
@@ -58,6 +59,7 @@ void make_acs_dataset(const Rcpp::CharacterVector & config_path) {
 	    }
 	}
 
+	std::cout << "Total records: " << acs_records.size() << std::endl;
 
     } catch (const std::runtime_error & e) {
 	Rcpp::Rcout << "Failed with error: " << e.what() << std::endl;
