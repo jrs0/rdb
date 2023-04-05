ba60a34cce854c040a95b47b9b8dfb7a78c18a51
John Scott <john.scott17@nhs.net>
2023-04-05 14:24:12 +0000

    Added tests for timestamp. Also found what looks like UB.
    Going to run valgrind.

--- src/acs.hpp
+++ src/acs.hpp
@@ -115,6 +115,7 @@ auto get_spells_in_window(const std::vector<Spell> & all_spells,
 	} else {
 	    bool a{(other_spell_start < base_start)
 		and (other_spell_start > base_start + offset_seconds)};
+	    std::cout << offset_seconds << std::endl;
 	    std::cout << other_spell_start << " " << base_start << " " << base_start + offset_seconds << " " << a << std::endl;
 	    return a;
 	}
---
+++ src/gtest/timestamp.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "../sql_types.hpp"
+
+TEST(Timestamp, DefaultConstructNull) {
+    Timestamp t;
+    EXPECT_TRUE(t.null());
+}
+
+TEST(Timestamp, SetInitialTimestamp) {
+    Timestamp t{300};
+    EXPECT_EQ(t.read(), 300);
+}
+
+TEST(Timestamp, OffsetBackwards) {
+    Timestamp t{600};
+    auto x{t + -200};
+    EXPECT_EQ(x.read(), 400);
+}
+
+TEST(Timestamp, OffsetForwards) {
+    Timestamp t{600};
+    auto x{t + 200};
+    EXPECT_EQ(x.read(), 800);
+}
+
+TEST(Timestamp, LargeOffsetBackwards) {
+    Timestamp t{1600473600}; // 2020-09-19  
+    auto x{t + -365*24*60*60}; // 1 year
+    EXPECT_EQ(x.read(), 1568937600);
+}
+
+TEST(Timestamp, LargeOffsetForwards) {
+    Timestamp t{1600473600}; // 2020-09-19  
+    auto x{t + 365*24*60*60}; // 1 year
+    EXPECT_EQ(x.read(), 1632009600);
+}
