03c7330dd7c13c9f50edb5124219bd70d0ae61ba
John Scott <john.scott17@nhs.net>
2023-04-05 14:11:57 +0000

    Found a bug in the value of timestamp. Going to try to isolate in a test

--- src/acs.hpp
+++ src/acs.hpp
@@ -115,7 +115,7 @@ auto get_spells_in_window(const std::vector<Spell> & all_spells,
 	} else {
 	    bool a{(other_spell_start < base_start)
 		and (other_spell_start > base_start + offset_seconds)};
-	    std::cout << other_spell_start << " " << base_start << " " << a << std::endl;
+	    std::cout << other_spell_start << " " << base_start << " " << base_start + offset_seconds << " " << a << std::endl;
 	    return a;
 	}
     }};
