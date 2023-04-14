6b63d134e215de36aad0db21f701fa429d9288c6
John Scott <john.scott17@nhs.net>
2023-04-14 14:45:37 +0100

    Started adding some simple descriptive functions for mortality

--- scripts/analysis/model_test.R
+++ scripts/analysis/model_test.R
@@ -48,8 +48,51 @@ without_near_zero_variance_columns <- function(recipe) {
         select(retained_cols)
 }
 
+percentages_by_outcome <- function(dataset, pred, column_name) {
+    dataset %>%
+        group_by(`Bleeding After` = bleeding_after != 0, `Ischaemia After` = ischaemia_after != 0) %>%
+        summarize({{ column_name }} := percent(mean({{ pred }}))) %>%
+        arrange({{ column_name }})
+}
+
+percentages_by_stemi <- function(dataset, pred, column_name) {
+    dataset %>%
+        group_by(Presentation = stemi_presentation) %>%
+        summarize({{ column_name }} := percent(mean({{ pred }}))) %>%
+        arrange({{ column_name }})
+}
+
+
+presented_stemi_by_outcome <- raw_dataset %>%
+    percentages_by_outcome(stemi_presentation == "STEMI",
+                         `Presented STEMI`)
+
+had_pci_by_outcome <- raw_dataset %>%
+    percentages_by_outcome(index_type == "PCI",
+                         `Had PCI`)
+
+any_cause_death_by_outcome <- raw_dataset %>%
+    percentages_by_outcome(cause_of_death != "no_death",
+                         `Any Cause Death`)
+
+cardiovascular_death_by_outcome <- raw_dataset %>%
+    percentages_by_outcome(cause_of_death == "cardiac",
+                         `Cardiovascular Death`)
+
+cardiovascular_death_by_presentation <- raw_dataset %>%
+    percentages_by_stemi(cause_of_death == "cardiac",
+                         `Cardiovascular Death`)
+
+any_cause_death_by_presentation <- raw_dataset %>%
+    percentages_by_stemi(cause_of_death != "no_death",
+                         `Any Cause Death`)
+
+
+
 raw_dataset <- processed_acs_dataset("../../config.yaml")
 
+## Simple proportions from the data
+    
 dataset <- raw_dataset %>%
     mutate(bleeding_after = factor(bleeding_after == 0, labels = c("bleeding_occurred", "no_bleed"))) %>%
     mutate(ischaemia_after = factor(ischaemia_after == 0, labels = c("ischaemia_occurred", "no_ischaemia"))) %>%
