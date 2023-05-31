b1dd7a0aabbfe826b3631bad38c48ace4e1a7fb5
John Scott <john.scott17@nhs.net>
2023-05-31 11:42:16 +0100

    Got the primary care attributes joined to the acs index events

--- scripts/analysis/swd_dataset.R
+++ scripts/analysis/swd_dataset.R
@@ -10,7 +10,25 @@ msrv <- mapped_server("xsw")
 
 ## Load the raw data from file or database
 raw_dataset <- processed_acs_dataset("../../scripts/config.yaml")
+raw_dataset
 
-
+## Doing this as a right_join to copy data to the remote sql as a temporary
+## table. Doing this is faster than just fetching all the data (30 seconds
+## vs 10 minutes). The inner join excludes nhs_numbers which have no row at
+## all in the swd (in order to interpret subsequent NA as lack of condition
+## rather than missingness)
+nhs_numbers <- raw_dataset %>%
+    distinct(nhs_number)
 primary_care_attribute_history <- msrv$swd$swd_attribute_history %>%
+    inner_join(nhs_numbers, by = "nhs_number", copy = TRUE) %>%
     run()
+
+## Join the patient attributes to the acs index events, reducing to only
+## the nhs_numbers with a row in the primary care attributes.
+raw_dataset %>%
+    mutate(index_id = row_number()) %>%
+    inner_join(primary_care_attribute_history,
+              by = "nhs_number",
+              relationship = "many-to-many") %>%
+    count(index_id) %>%
+    arrange(desc(n))
