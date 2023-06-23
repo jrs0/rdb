b2a95771f8165d1f2841382b71b33837191ba312
John Scott <john.scott17@nhs.net>
2023-06-23 09:59:00 +0100

    Connected/disconnected successfully

--- src/programs/test.cpp
+++ src/programs/test.cpp
@@ -1,5 +1,15 @@
+#define OTL_ODBC_MSSQL_2008 // Compile OTL 4/ODBC, MS SQL 2008
 #include "otlv4.h"
 
 int main() {
+
+    otl_connect::otl_initialize();
+
+    otl_connect db;
+    db.rlogon("DSN=xsw");
+
+
+    db.logoff();
+    std::cout << "Got to the end" << std::endl;
 
 }
